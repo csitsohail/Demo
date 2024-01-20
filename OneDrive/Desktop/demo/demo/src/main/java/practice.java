544a8572a30ec921f0eaae95c0549a1aeff307ff
Md Sohail <csitsohail@gmail.com>
2024-01-20 18:27:48 +0530

    reverse a string file

---
+++ OneDrive/Desktop/demo/demo/src/main/java/practice.java
@@ -0,0 +1,16 @@
+public class practice {
+    public static void main(String[] args) {
+                String originalString = "Hello, World!";
+
+                // Using Java Stream API to reverse the string
+                String reversedString = new StringBuilder(originalString)
+                        .reverse()
+                        .toString();
+
+                System.out.println("Original String: " + originalString);
+                System.out.println("Reversed String: " + reversedString);
+            }
+        }
+
+
+
